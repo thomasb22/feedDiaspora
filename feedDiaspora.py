bdff2a8ddff086be958badd1f3d54c8a688a66a6
Thomas Blanschong <contact@thomasblanschong.fr>
2023-11-09 18:43:19 +0100

    Multi feed

--- feedDiaspora.py
+++ feedDiaspora.py
@@ -12,15 +12,16 @@ __pod__ = 'https://diasporafoundation.org'
 __username__ = ''
 __passwd__ = ''
 
-feedurl = 'https://example.org/feed.xml'
+feedsUrl = ['http://exemple.com/rss1.xml', 'http://exemple.com/rss2.xml']
 hashtags = '#feedDiaspora #Diaspora'
 
 database = 'feedDiaspora-db.txt'
 tmpdir = 'tmp'
-show_summary = False
-show_picture = False
-maxmsg = 2
-maxchar = 1000
+show_summary = [False, False]
+show_picture = [True, False]
+maxmsg = [2, 2]
+maxchar = [1000, 1000]
+logged = False
 
 c = diaspy.connection.Connection(
 	pod=__pod__,
@@ -28,90 +29,92 @@ c = diaspy.connection.Connection(
 	password=__passwd__
 )
 
-nbmsg = 0
-feed = feedparser.parse(feedurl)
+for idx, feedUrl in enumerate(feedsUrl):
+	nbmsg = 0
+	feed = feedparser.parse(feedUrl)
 
-for item in reversed(feed.entries):
-	send = True
-	soup = BeautifulSoup(item.title, 'lxml')
-	title = soup.text
-	soup = BeautifulSoup(item.summary, 'lxml')
-	summary = soup.text
-	link = item.link
-	msg = '### ' + title + '\n\n' + link
+	for item in reversed(feed.entries):
+		send = True
+		soup = BeautifulSoup(item.title, 'lxml')
+		title = soup.text
+		soup = BeautifulSoup(item.summary, 'lxml')
+		summary = soup.text
+		link = item.link
+		msg = '### ' + title + '\n\n' + link
 
-	if show_summary:
-		summary = '> '.join( ( '\n' + summary.lstrip() ).splitlines(True) )
-		msg = '### ' + title + '\n\n' + summary + '\n\n' + link
+		if show_summary[idx]:
+			summary = '> '.join( ( '\n' + summary.lstrip() ).splitlines(True) )
+			msg = '### ' + title + '\n\n' + summary + '\n\n' + link
 
-	if hashtags:
-		msg += '\n\n' + hashtags
-
-	if show_summary and len(msg) > maxchar and len(summary) > (len(msg) - maxchar) - 2:
 		if hashtags:
-			maxsum = len(summary) - (len(msg) - maxchar) - 2
-		else:
-			maxsum = len(summary) - (len(msg) - maxchar) - 1
-		msg = title + '\n\n' + summary[:maxsum] + '…\n\n' + link
-
-		if hashtags and len(msg) <= maxchar - (len(hashtags) + 1):
 			msg += '\n\n' + hashtags
-	elif len(msg) > maxchar and len(title) > (len(msg) - maxchar) - 1:
-		maxtitle = len(title) - (len(msg) - maxchar) - 1
-		msg = title[:maxtitle] + '… ' + link
 
-		if hashtags and len(msg) <= maxchar - (len(hashtags) + 1):
-			msg += ' ' + hashtags
+		if show_summary[idx] and len(msg) > maxchar[idx] and len(summary) > (len(msg) - maxchar[idx]) - 2:
+			if hashtags:
+				maxsum = len(summary) - (len(msg) - maxchar[idx]) - 2
+			else:
+				maxsum = len(summary) - (len(msg) - maxchar[idx]) - 1
+			msg = title + '\n\n' + summary[:maxsum] + '…\n\n' + link
 
-	if len(msg) > maxchar:
-		send = False
+			if hashtags and len(msg) <= maxchar[idx] - (len(hashtags) + 1):
+				msg += '\n\n' + hashtags
+		elif len(msg) > maxchar[idx] and len(title) > (len(msg) - maxchar[idx]) - 1:
+			maxtitle = len(title) - (len(msg) - maxchar[idx]) - 1
+			msg = title[:maxtitle] + '… ' + link
 
-	if os.path.exists(database):
-		db = open(database, "r+")
-		entries = db.readlines()
-	else:
-		db = open(database, "a+")
-		entries = []
+			if hashtags and len(msg) <= maxchar[idx] - (len(hashtags) + 1):
+				msg += ' ' + hashtags
 
-	for entry in entries:
-		if link in entry:
+		if len(msg) > maxchar[idx]:
 			send = False
 
-	if send:
-		if nbmsg == 0:
-			c.login()
-			stream = diaspy.streams.Stream(c)
-
-		if show_picture and item.enclosures:
-			if item.enclosures[0].type[:5] == 'image' and int(item.enclosures[0].length) <= 1000000:
-				tmpfilename = item.enclosures[0].href.split('/')[-1]
-				tmppath = tmpdir + '/' + tmpfilename
-				picture = tmppath
-
-				if not os.path.exists(tmpdir):
-					os.mkdir(tmpdir)
-
-				wget.download(item.enclosures[0].href, tmppath)
-
-				try:
-					stream.post(text=msg, photo=picture)
-				except (diaspy.errors.StreamError) as e:
-					warnings.warn('{0}')
-				finally:
-					pass
-
-				os.remove(tmppath)
+		if os.path.exists(database):
+			db = open(database, "r+")
+			entries = db.readlines()
 		else:
-			stream.post(msg)
+			db = open(database, "a+")
+			entries = []
 
-		db.write(link + '\n')
-		db.flush()
+		for entry in entries:
+			if link in entry:
+				send = False
 
-		nbmsg = nbmsg + 1
-		if nbmsg >= maxmsg:
-			break
+		if send:
+			if not logged:
+				c.login()
+				stream = diaspy.streams.Stream(c)
+				logged = True
 
-	db.close()
+			if show_picture[idx] and item.enclosures:
+				if item.enclosures[0].type[:5] == 'image' and int(item.enclosures[0].length) <= 1000000:
+					tmpfilename = item.enclosures[0].href.split('/')[-1]
+					tmppath = tmpdir + '/' + tmpfilename
+					picture = tmppath
+
+					if not os.path.exists(tmpdir):
+						os.mkdir(tmpdir)
+
+					wget.download(item.enclosures[0].href, tmppath)
+
+					try:
+						stream.post(text=msg, photo=picture)
+					except (diaspy.errors.StreamError) as e:
+						warnings.warn('{0}')
+					finally:
+						pass
+
+					os.remove(tmppath)
+			else:
+				stream.post(msg)
+
+			db.write(link + '\n')
+			db.flush()
+
+			nbmsg = nbmsg + 1
+			if nbmsg >= maxmsg[idx]:
+				break
+
+		db.close()
 
 if os.path.exists(tmpdir):
 	os.rmdir(tmpdir)
